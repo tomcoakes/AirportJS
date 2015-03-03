e997e38c5fc0d9341d551402ffeda07009c04cf6
Tom Coakes <tomcoakes@gmail.com>
2015-03-03 18:29:45 +0000

    Airport _dock is now a private method that is only used by Airport.land

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -5,6 +5,12 @@ describe("Airport", function() {
     plane = jasmine.createSpyObj("plane", ['land','takeOff']);
   });
 
+  function fillAirport() {
+    for (i = 0; i < 10; i++) {
+      airport.land(plane);
+    };
+  };
+
   it("can land a plane", function() {
     airport.land(plane);
     expect(plane.land).toHaveBeenCalled();
@@ -12,20 +18,17 @@ describe("Airport", function() {
 
   it("can dock a plane", function() {
     airport.land(plane);
-    airport.dock(plane);
     expect(airport.planes.length).toEqual(1);
   });
 
   it("can allow a plane to take off", function() {
     airport.land(plane);
-    airport.dock(plane);
     airport.takeOff(plane);
     expect(plane.takeOff).toHaveBeenCalled();
   });
 
   it("knows a plane has taken off", function() {
     airport.land(plane);
-    airport.dock(plane);
     airport.takeOff(plane);
     expect(airport.planes.length).toEqual(0);
   });
--- src/Airport.js
+++ src/Airport.js
@@ -4,14 +4,19 @@ var Airport = function() {
 
   Airport.prototype.land = function(plane) {
     plane.land();
+    this._dock(plane);
   };
 
-  Airport.prototype.dock = function(plane) {
-    this.planes.push(plane);
-  };
+
 
   Airport.prototype.takeOff = function(plane) {
     plane.takeOff();
     var planeIndex = this.planes.indexOf(plane);
     this.planes.splice(planeIndex, 1);
+  };
+
+// Private method
+
+  Airport.prototype._dock = function(plane) {
+    this.planes.push(plane);
   };
