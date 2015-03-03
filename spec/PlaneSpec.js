af8325011849c52f27cfbc6554b7dcfdf74ce150
Tom Coakes <tomcoakes@gmail.com>
2015-03-03 16:45:08 +0000

    Planes can land and takeoff

--- spec/PlaneSpec.js
+++ spec/PlaneSpec.js
@@ -2,7 +2,18 @@ describe("Plane", function() {
 
   var plane = new Plane();
 
-  it("is flying apon creation", function() {
+  it("is flying upon creation", function() {
+    expect(plane.flying).toBe(true);
+  });
+
+  it("can land", function() {
+    plane.land();
+    expect(plane.flying).toBe(false);
+  });
+
+  it("can takeoff", function() {
+    plane.land();
+    plane.takeOff();
     expect(plane.flying).toBe(true);
   });
 
--- src/Plane.js
+++ src/Plane.js
@@ -2,3 +2,10 @@ var Plane = function() {
   this.flying = true;
 };
 
+Plane.prototype.land = function() {
+  this.flying = false;
+};
+
+Plane.prototype.takeOff = function() {
+  this.flying = true;
+};
